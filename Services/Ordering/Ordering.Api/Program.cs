a6ba7a5085e911a9f7408f067fa4a9f194ecd511
aliw-md <aliw.md1@gmail.com>
2022-08-20 02:30:10 +0430

    Configuring sqlserver in docker. toplearn course - 55

--- Services/Ordering/Ordering.Api/Program.cs
+++ Services/Ordering/Ordering.Api/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Ordering.Api.Extensions;
+using Ordering.Infrastructure.Persistence;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Api
 {
@@ -13,9 +11,14 @@ namespace Ordering.Api
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-
-            host.Run();
+            CreateHostBuilder(args)
+                .Build()
+                .MigrateDatabase<OrderContext>((context, services) =>
+                {
+                    var logger = services.GetService<ILogger<OrderContextSeed>>();
+                    OrderContextSeed.SeedAsync(context, logger).Wait();
+                })
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
